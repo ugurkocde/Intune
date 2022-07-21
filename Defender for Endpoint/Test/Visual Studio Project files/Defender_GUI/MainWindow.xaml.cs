98b2d0b249c06efb00b5ffb9dd12af76d97b18a4
Ugur Koc <43906965+ugurkocde@users.noreply.github.com>
2022-07-21 10:23:02 +0200

    upload vs project files

---
+++ Defender for Endpoint/Test/Visual Studio Project files/Defender_GUI/MainWindow.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Defender_GUI
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void microsoft_documentation_button_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void engine_reload_button_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void show_mde_logs_button_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
