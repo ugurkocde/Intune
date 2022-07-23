e843be39921ee32b0e4e5580a92675f4f1caa3d2
Ugur Koc <43906965+ugurkocde@users.noreply.github.com>
2022-07-23 21:23:25 +0200

    update version change to 1.1

--- Defender for Endpoint/MDE - Update Tool/MDE_Update_Tool.ps1
+++ Defender for Endpoint/MDE - Update Tool/MDE_Update_Tool.ps1
@@ -1,6 +1,6 @@
 # Author: Ugur Koc
 # Created: Init 21.07.2022
-# Version: 1.0
+# Version: 1.1
 # Blogpost: https://ugurkoc.de/microsoft-defender-for-endpoint-mde-update-tool/
 # Website: ugurkoc.de
 # Twitter: @ugurkocde
