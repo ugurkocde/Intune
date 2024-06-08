d151f1eef11f925c2d80220cf8182dc13c195bf5
Ugur Koc | Microsoft MVP <43906965+ugurkocde@users.noreply.github.com>
2024-06-08 18:26:46 +0200

    as

--- Linux/postinstall.sh
+++ Linux/postinstall.sh
@@ -1,28 +1,14 @@
 #!/bin/bash
 
-# Download the background image
-wget -O /usr/share/backgrounds/my_background.jpg https://raw.githubusercontent.com/ugurkocde/Intune/main/Linux/media/Thermenresort_Loipersdorf.jpg
+# Define variables
+IMAGE_URL="https://raw.githubusercontent.com/ugurkocde/Intune/main/Linux/media/Thermenresort_Loipersdorf.jpg"
+IMAGE_NAME="new_background.jpg"
+IMAGE_PATH="$HOME/Pictures/$IMAGE_NAME"
 
-# Create the set-background.service
-echo "[Unit]
-Description=Set Desktop Background
-After=graphical.target
+# Download the image
+wget -O "$IMAGE_PATH" "$IMAGE_URL"
 
-[Service]
-ExecStart=/usr/local/bin/set-background.sh
-Type=oneshot
+# Set the new desktop background
+gsettings set org.gnome.desktop.background picture-uri "file://$IMAGE_PATH"
 
-[Install]
-WantedBy=default.target" > /etc/systemd/system/set-background.service
-
-# Create the set-background.sh script
-echo "#!/bin/bash
-for user in \$(ls /home); do
-  sudo -u \$user DISPLAY=:0 DBUS_SESSION_BUS_ADDRESS=unix:path=/run/user/\$(id -u \$user)/bus gsettings set org.gnome.desktop.background picture-uri \"file:///usr/share/backgrounds/my_background.jpg\"
-done" > /usr/local/bin/set-background.sh
-
-# Make the set-background.sh script executable
-chmod +x /usr/local/bin/set-background.sh
-
-# Enable the set-background service
-systemctl enable set-background.service
+echo "Desktop background has been changed to the new image."
