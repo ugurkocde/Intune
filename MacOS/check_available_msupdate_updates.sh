85f7a30f731c3bb5e5832f8a5828eb76ce1568c8
Ugur Koc | Microsoft MVP <43906965+ugurkocde@users.noreply.github.com>
2025-01-08 13:17:52 +0100

    Update check_available_msupdate_updates.sh

--- MacOS/check_available_msupdate_updates.sh
+++ MacOS/check_available_msupdate_updates.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
-#
+
 # Description: This script displays available Updates for Microsoft Apps by checking MAU.
 #
+# For more scripts and guides on macOS and Intune, visit: IntuneMacAdmins.com
 
 # Path to msupdate CLI
 MSUPDATE="/Library/Application Support/Microsoft/MAU2.0/Microsoft AutoUpdate.app/Contents/MacOS/msupdate"
